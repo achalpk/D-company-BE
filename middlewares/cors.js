818aed441055dd732a22992e968f8d5293e0b72e
achalpk <mail.achalpk@gmail.com>
2022-09-26 10:19:35 +0530

    cors fixed

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,6 @@
 const cors = (req, res, next)=>{
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', '*');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 }
